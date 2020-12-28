9167e17476a7a5a3ddf0b3f84101e5c91e35272a
Nico Prasetyo <prasetyon100402@gmail.com>
2020-12-28 21:10:45 +0700

    rename menu utama

--- Battle System/Form2.Designer.cs
+++ Battle System/Form2.Designer.cs
@@ -28,19 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnPlay = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(354, 337);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "PLAY!";
+            this.btnPlay.UseVisualStyleBackColor = true;
             // 
             // FormMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPlay);
             this.Name = "FormMenu";
-            this.Text = "Form2";
+            this.Text = "BATTLE SYSTEM";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnPlay;
     }
 }
