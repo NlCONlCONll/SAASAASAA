7db206c47104de7b143502d0290b6d60a6080fb2
Nico Prasetyo <prasetyon100402@gmail.com>
2020-12-28 22:21:26 +0700

    added play button
    added play button via github desktop

--- Battle System/Form2.Designer.cs
+++ Battle System/Form2.Designer.cs
@@ -29,32 +29,46 @@
         private void InitializeComponent()
         {
             this.btnPlay = new System.Windows.Forms.Button();
+            this.lblNamaGame = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnPlay
             // 
-            this.btnPlay.Location = new System.Drawing.Point(354, 337);
+            this.btnPlay.Location = new System.Drawing.Point(325, 332);
             this.btnPlay.Name = "btnPlay";
-            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.Size = new System.Drawing.Size(157, 52);
             this.btnPlay.TabIndex = 0;
             this.btnPlay.Text = "PLAY!";
             this.btnPlay.UseVisualStyleBackColor = true;
+            // 
+            // lblNamaGame
+            // 
+            this.lblNamaGame.AutoSize = true;
+            this.lblNamaGame.Location = new System.Drawing.Point(354, 177);
+            this.lblNamaGame.Name = "lblNamaGame";
+            this.lblNamaGame.Size = new System.Drawing.Size(91, 17);
+            this.lblNamaGame.TabIndex = 1;
+            this.lblNamaGame.Text = "NAMA GAME";
+            this.lblNamaGame.Click += new System.EventHandler(this.label1_Click);
             // 
             // FormMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNamaGame);
             this.Controls.Add(this.btnPlay);
             this.Name = "FormMenu";
             this.Text = "BATTLE SYSTEM";
             this.Load += new System.EventHandler(this.FormMenu_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label lblNamaGame;
     }
 }
--- Battle System/Form2.cs
+++ Battle System/Form2.cs
@@ -21,5 +21,10 @@ namespace Battle_System
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
